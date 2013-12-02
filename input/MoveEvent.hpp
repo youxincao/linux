ecce19b963c05d8d81dc6e3d42c6d2a0b6f0249c
weilun <lun.wei@outlook.com>
2013-12-02 15:54:24 +0800

    1 : Add more point in the move event . 2 : Add the multi program test .

--- input/MoveEvent.hpp
+++ input/MoveEvent.hpp
@@ -25,6 +25,10 @@ shared_ptr<InputOperation> MoveEvent::generateOperation( int fd )
 {
     shared_ptr<InputOperation> pOperation( new InputOperation ( fd ) ) ;
 
+    int middle_x = (mBegin.getPositionX() + mEnd.getPositionX() ) / 2 ;
+    int middle_y = (mBegin.getPositionY() + mEnd.getPositionY() ) / 2 ;
+
+
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_X , mBegin.getPositionX() ));
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_Y , mBegin.getPositionY() ));
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TRACKING_ID , 0 ));
@@ -32,7 +36,34 @@ shared_ptr<InputOperation> MoveEvent::generateOperation( int fd )
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_WIDTH_MAJOR , 0 ));
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
- 
+
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_X , ( mBegin.getPositionX() + middle_x ) / 2 ) ) ;
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_Y , ( mBegin.getPositionY() + middle_y ) / 2 ) ) ;
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TRACKING_ID , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TOUCH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_WIDTH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
+
+    
+
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_X , middle_x ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_Y , middle_y ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TRACKING_ID , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TOUCH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_WIDTH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
+
+   
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_X , ( mEnd.getPositionX() + middle_x ) / 2 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_Y , ( mEnd.getPositionY() + middle_y ) / 2 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TRACKING_ID , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TOUCH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_WIDTH_MAJOR , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
+    pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
+
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_X , mEnd.getPositionX() ));
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_POSITION_Y , mEnd.getPositionY() ));
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_TRACKING_ID , 0 ));
@@ -40,7 +71,7 @@ shared_ptr<InputOperation> MoveEvent::generateOperation( int fd )
     pOperation->addInputEvent( InputEvent( EV_ABS , ABS_MT_WIDTH_MAJOR , 0 ));
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
-    
+
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_MT_REPORT , 0 ));
     pOperation->addInputEvent( InputEvent( EV_SYN , SYN_REPORT , 0 ));
     
---
+++ input/TestMul.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+
+#include "InputEvent.hpp"
+#include "InputOperation.hpp"
+#include "TouchEvent.hpp"
+#include "MoveEvent.hpp"
+
+using namespace std;
+
+int openInputDevice( int num )
+{
+    char name[64];
+    int ret = 0 ;
+    
+    sprintf( name , "/dev/input/event%d" , num );
+
+    ret = open( name , O_RDWR , 0 );
+    if ( ret < 0)
+    {
+        cerr << "Open " << name << " failed " << endl ;
+        return -1 ; 
+    }
+    return ret ;
+}
+
+int main(int argc, char *argv[])
+{
+    int screenFd = openInputDevice( 0 ) ;
+
+    if ( screenFd < 0   )
+    {
+        return -1 ; 
+    }
+
+    int sleepTime = 1 ;
+    int count = 1 ; 
+   
+    printf("Input Enter Count : ");
+    scanf( "%d" , &count );
+    printf("Input Sleep Time : " );
+    scanf( "%d" , &sleepTime );
+
+   
+    TouchEvent touchEvent1( Position( 62 , 88 ));
+    shared_ptr<InputOperation> op1 = touchEvent1.generateOperation( screenFd ) ;
+
+    TouchEvent touchEvent2( Position( 154 , 88 ));
+    shared_ptr<InputOperation> op2 = touchEvent2.generateOperation( screenFd ) ;
+
+    TouchEvent touchEvent3( Position( 86 , 172 ));
+    shared_ptr<InputOperation> op3 = touchEvent3.generateOperation( screenFd ) ;
+
+    TouchEvent touchEvent4( Position( 176 , 172 ));
+    shared_ptr<InputOperation> op4 = touchEvent4.generateOperation( screenFd ) ;
+
+    
+    MoveEvent moveRightEvent( Position( 92 , 150  ) , Position( 195 , 150 ));
+    shared_ptr<InputOperation> moveRightOp = moveRightEvent.generateOperation( screenFd ) ;
+
+    MoveEvent moveLeftEvent( Position( 180 , 120  ) , Position( 44 , 120 ));
+    shared_ptr<InputOperation> moveLeftOp = moveLeftEvent.generateOperation( screenFd ) ;
+
+    for (int i = 0; i < count; ++i)
+    {
+        printf("The count %d \n" , i );
+
+        for( int j = 0 ; j < 3 ;  j ++ )
+        {
+            
+            op1->execute() ; 
+            sleep( sleepTime );
+            moveRightOp->execute() ; 
+            sleep( sleepTime );
+
+            
+            op2->execute() ; 
+            sleep( sleepTime );
+            moveRightOp->execute() ; 
+            sleep( sleepTime );
+
+
+            op3->execute() ; 
+            sleep( sleepTime );
+            moveRightOp->execute() ; 
+            sleep( sleepTime );
+
+            op4->execute() ; 
+            sleep( sleepTime );
+            moveRightOp->execute() ; 
+            sleep( sleepTime );
+
+            moveLeftOp->execute() ;
+            sleep(sleepTime );
+        }
+
+        moveRightOp->execute() ;
+        sleep( 2 ) ;
+        moveRightOp->execute( ) ;
+        sleep( 2 ) ;
+        
+    }
+
+    
+    close( screenFd );
+    return 0;
+}
