efb01dc6ebd90cdd61e3c21b194d0e91cb81d4b0
weilun <lun.wei@outlook.com>
2013-12-02 12:22:46 +0800

    1:send packet and recv packet and test it

--- bluetooth/rfcomm_client.c
+++ bluetooth/rfcomm_client.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/rfcomm.h>
 
+// The socket 
+static int s ;
+static int gPacketSize ;
+static int gPacketCount ;
+static int gSleepTime ; 
+static char* gSendBuf ;
+static char* gRecvBuf ; 
 
+#define MAX_PACKET_SIZE 725
 
 int main(int argc, char **argv)
 {
     struct sockaddr_rc addr = { 0 };
-    int s, status;
+    int  status;
     char dest[18] = "00:1A:7D:DA:71:06";
 
     if( argc != 2 )
     {
-        fprintf( stderr , "Please give us a bluetooth address !") ;
+        fprintf( stderr , "Please give us a bluetooth address ! \n") ;
         return -1 ; 
     }
 
@@ -40,20 +50,70 @@ int main(int argc, char **argv)
     status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 	char buf[512] = { 0  };
 
+    // get the test parameter
+    printf("Packet Size : " );
+    scanf("%d" , &gPacketSize );
+    printf("Packet Count :");
+    scanf("%d" , &gPacketCount );
+    printf("Sleep time : ");
+    scanf("%d" , &gSleepTime );
+
+    if( gPacketSize > MAX_PACKET_SIZE || gPacketSize <= 0 )
+        gPacketSize = MAX_PACKET_SIZE ;
+
+    // malloc the buffer 
+    gSendBuf = ( char * )malloc( gPacketSize * sizeof( char )) ;
+    if (gSendBuf == NULL )
+    {
+        fprintf( stderr , "Malloc the send buffer failed \n");
+        return -1 ; 
+    }
+
+    gRecvBuf = ( char * )malloc( gPacketSize * sizeof( char )) ;
+    if ( gRecvBuf == NULL )
+    {
+        fprintf( stderr , "Malloc the send buffer failed \n");
+        return -1 ; 
+    }
+
+    // Initilie the send buf
+    for ( int i = 0  ;  i < gPacketSize ; ++i)
+    {
+        gSendBuf[ i  ] = ( char ) i % 256 ; 
+    }
+
+    int err_count = 0 ; 
     // send a message
     if( status == 0 ) {
-		while ( 1  )
-		{
-			printf("[rfcomm_clint] : send Hello \n" );
-    	    status = write(s, "hello!", 6);
-			read( s , buf  , 512  );
-			printf("[rfcomm_clint] : recv %s \n" , buf ) ;
-			sleep( 2  );
-		}		
+       
+		for (int i = 0; i < gPacketCount; ++i)
+        {
+            for ( int i = 0  ;  i < gPacketSize ; ++i)
+            {
+                gSendBuf[ i  ] = ( char ) i % 256 ; 
+            }
+            status = write(s, gSendBuf ,  gPacketSize );
+            int redLen = read( s , gRecvBuf  , gPacketSize );            
+			sleep( gSleepTime );
+
+            int ret = memcmp( gSendBuf , gRecvBuf , gPacketSize ) ;
+            if( ret != 0 )
+            {
+                printf("%d FALSE\n" , i  ) ;
+                err_count ++ ; 
+            }
+            else
+            {
+                printf("%d OK \n" , i );
+            }
+        }
     }
 
     if( status < 0 ) perror("uh oh");
 
+    printf("Send %d Packets(size=%d) , err %d \n" ,
+           gPacketCount , gPacketSize , err_count );
+    
     close(s);
     return 0;
 }
